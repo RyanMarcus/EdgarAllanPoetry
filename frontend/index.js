ed818443169fa22e8873d6f53489989946166c98
Ryan Marcus <ryan@rmarcus.info>
2016-02-27 15:03:23 -0500

    changing host and port

--- frontend/index.js
+++ frontend/index.js
@@ -171,6 +171,6 @@ app.get('/scoreboard', function (req, res) {
     res.render('scoreboard', {"people": people});
 });
 
-app.listen(3000, function () {
+app.listen(3001, "127.0.0.1", function () {
     console.log('Example app listening on port 3000!');
 });
