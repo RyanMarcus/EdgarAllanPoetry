20f3df55c7a7273f44f8b3c31795d5a43c7d9dcf
Seth Rait <sethrait95@gmail.com>
2016-02-26 22:40:14 -0500

    whitespace removal

--- cleanse.py
+++ cleanse.py
@@ -1,10 +1,13 @@
 import sys
+import string
 for arg in sys.argv[1:]:
 	input_file = arg
 	f = open(input_file, 'r')
 	good = list("qwertyuiopasdfghjklzxcvbnm ?,")
 	for line in f:
-		s = line.lower()
-		s = [x for x in s if x in good]
-		print("".join(s))
-	f.close();
+		s = line.strip()
+		if len(s) != 0:
+			s = line.lower()
+			s = [x for x in s if x in good]
+			print(("".join(s)).strip(' \t\n\r'))
+	f.close()
