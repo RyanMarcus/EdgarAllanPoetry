8ea7f7b7f481acdd59dcf1dcdae71453cc0617c5
Sofiya Semenova <novasofiya@gmail.com>
2016-02-26 21:51:22 -0500

    bla

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+const pythonShell = require('python-shell');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -9,7 +10,11 @@ app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
 
 app.get('/', function (req, res) {
-  res.render('turing', {});
+  var options = { pythonPath: 'python3'}
+  pythonShell.run('generate_poem.py', options, function(err, poem) {
+    // if (err) throw err;
+    res.render('turing', {poem});
+  });
 });
 
 app.listen(3000, function () {
