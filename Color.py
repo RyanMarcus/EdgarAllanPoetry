a543922adb5f9fe6612cb5290a6dbe49480e124c
Seth Rait <sethrait95@gmail.com>
2016-02-27 10:41:57 -0500

    granularized color vals

--- Color.py
+++ Color.py
@@ -1,15 +1,5 @@
 import indicoio as ind
 from sys import stdin
 ind.config.api_key = 'b88a14d4a97b56a6ed8f65efee05f9c4'
-happy_thresh = 0.75
-sad_thresh = 0.25
 str = stdin.read()
-val = ind.sentiment_hq(str)
-if val > sad_thresh:
-	if val < happy_thresh:
-		print("m")
-	else:
-		print("t")
-else:
-	print("f")
-print(val)
+print(ind.sentiment_hq(str))
