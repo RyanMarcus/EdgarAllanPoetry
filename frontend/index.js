414020320d1f3d986e03c3dc41b50824c994c595
Luka Milekic <lmilekic@gmail.com>
2016-02-27 10:52:48 -0500

    added ajax

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static("css"));
-
+app.use(express.static('public'));
 const pythonShell = require('python-shell');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,23 +10,69 @@ const mustache = require('mustache-express');
 app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
 
+var testing = true;
+
+var dict = {};
+
+dict['current_id'] = 0;
+dict['correct_ids'] = new Set();
+
 app.get('/', function (req, res) {
   var options = { pythonPath: 'python3'}
   pythonShell.run('generate_poem.py', options, function(err, fakePoem) {
     pythonShell.run('pick_selection.py', options, function(err, realPoem) {
       flip = Math.floor((Math.random() * 2));
+      var current_id = dict['current_id']
+      dict['current_id'] = current_id + 2;
+      poem1_id = current_id + 1;
+      poem2_id = current_id + 2;
+
       if (flip==1) {
+        dict['correct_ids'].add(poem2_id)
         poem1 = fakePoem;
         poem2 = realPoem;
       } else {
+        dict['correct_ids'].add(poem1_id)
         poem1 = realPoem;
         poem2 = fakePoem;
       }
-      res.render('turing', {"poem1": poem1, "poem2": poem2, "flip": flip});
+      res.render('turing', {"poem1": poem1, "poem2": poem2, "flip": flip, "poem1_id": poem1_id.toString(), "poem2_id": poem2_id.toString()});
     })
   });
 });
 
+app.post('/ajaxSendData', function(req, res) {
+  res.send(req.body);
+  //res.send(true);
+});
+app.get('/ajaxGetData', function(req, res){
+//Copied from '/' because I didn't want to make 3 1 line methods
+  var options = { pythonPath: 'python3'}
+  pythonShell.run('generate_poem.py', options, function(err, fakePoem) {
+    pythonShell.run('pick_selection.py', options, function(err, realPoem) {
+      flip = Math.floor((Math.random() * 2));
+      var current_id = 0
+      current_id = dict['current_id']
+      dict['current_id'] = current_id + 2
+      poem1_id = current_id + 1;
+      poem2_id = current_id + 2;
+
+      if (flip==1) {
+        dict['correct_ids'].add(poem2_id)
+        poem1 = fakePoem;
+        poem2 = realPoem;
+      } else {
+        dict['correct_ids'].add(poem1_id)
+        poem1 = realPoem;
+        poem2 = fakePoem;
+      }
+      console.log("poem ids:");
+      console.log(dict['correct_ids'])
+      var result = poem1+'#'+poem2+'#' + poem1_id + '#' + poem2_id;
+      res.send(result);
+    })
+  });
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
